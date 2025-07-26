42c45b03829d342ab7618ac55f2e5b22d735d637
dylanmagumura <dmagumura@byupathway.edu>
2025-07-27 00:25:17 +0200

    Add files via upload
    Signed-off-by: dylanmagumura <dmagumura@byupathway.edu>

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,7 @@
+// getdates.js
+
+// Set current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set last modified date
+document.getElementById("lastModified").textContent = "Last Updated: " + new Date(document.lastModified).toLocaleDateString();
